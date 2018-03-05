c3ac13056a517156e189e4109151341db0d0d931
yili_ar <1044686276@qq.com>
2018-03-05 16:54:55 +0800

    Add files via upload

---
+++ duijiao.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class duijiao : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		#if UNITY_EDITOR  
+		if(Input.GetMouseButtonUp(0))  
+		#elif UNITY_ANDROID || UNITY_IPHONE  
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)  
+		#endif  
+		{  
+			Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);  
+		}  
+	}
+}
